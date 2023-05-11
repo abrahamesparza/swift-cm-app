69f822fb93508315d452d994b6aecad18dcb7268
Abraham Esparza <abrahamesparza.dev@gmail.com>
2023-05-11 13:50:53 -0700

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Abraham Esparza on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
